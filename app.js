1c4ca433554f816e1084f3b248e122cce55e9cfa
cfoulk <charlesfoulk@csus.edu>
2021-11-14 11:08:01 -0800

    add todos working

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
-const todoList = document.querySelector(".todo");
+const todoList = document.querySelector(".todo-list");
 
-document.addEventListener("DOMContentLoaded", getTodos);
+//document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodo);
+
+function addTodo(e) {
+  e.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo-list");
+  const li = document.createElement("li");
+  li.innerText = todoInput.value;
+
+  li.classList.add("todo-item");
+  todoDiv.appendChild(li);
+  todoInput.value = "";
+  //localStorage.setItem("todos", JSON.stringify(li));
+  todoList.appendChild(todoDiv);
+}
 
 function getTodos() {
+  let todos;
+}
+
+function saveTodos() {
   let todos;
 }
