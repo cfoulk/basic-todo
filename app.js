e856b14d84c08fcbc3cfa2feeb8fb551f8b39070
cfoulk <charlesfoulk@csus.edu>
2021-11-14 08:15:18 -0800

    simple styling

---
+++ app.js
@@ -0,0 +1,9 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo");
+
+document.addEventListener("DOMContentLoaded", getTodos);
+
+function getTodos() {
+  let todos;
+}
